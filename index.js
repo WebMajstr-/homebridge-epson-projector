85d124fe9c4fa2cb7fae44cca281b8fbcb0cda60
Nace Hudobivnik <nace@hudobivnik.si>
2023-01-28 15:03:37 +0100

    Rework plugin to support my custom IR server as Epson is not connected to network when powered off

--- index.js
+++ index.js
@@ -4,23 +4,25 @@ const command_path = "/cgi-bin/directsend?";
 
 const query_path = "/cgi-bin/json_query?jsoncallback=";
 
-const timeout = 5000;
-
-const debug = false;
+// High enough timeout on my network seems to be around 50ms for my projector. So 1.5s feels more than enough.
+const timeout = 1500;
 
 var Service;
 var Characteristic;
+var debug;
 
 module.exports = function (homebridge) {
     Service = homebridge.hap.Service;
     Characteristic = homebridge.hap.Characteristic;
-    homebridge.registerAccessory("homebridge-epson-projector", "EpsonProjector", EpsonProjector);
+    homebridge.registerAccessory("homebridge-epson-projector-api-and-ir", "EpsonProjector", EpsonProjector);
 };
 
 
 function EpsonProjector(log, config) {
     this.log = log;
+    debug = config["debug"];
     this.ip = config["ip"];
+    this.irip = config["irip"];
     this.model = config["model"] === undefined ? "" : config["model"];
     this.serial = config["serial"] === undefined ? "" : config["serial"];
     this.name = config["name"];
@@ -29,8 +31,9 @@ function EpsonProjector(log, config) {
 EpsonProjector.prototype = {
 
     getPowerState: function (callback) {
+
         if (debug) {
-            console.log(error);
+            console.log("Get power state for Epson Projector");
         }
         request.get({
             uri: "http://" + this.ip + query_path + "PWR?",
@@ -40,29 +43,39 @@ EpsonProjector.prototype = {
             timeout: timeout
         }, function (error, response, body) {
             if (error !== null) {
-                callback(error);
+                if (debug) {
+                    console.log(error)
+                }
+                callback(null, false) // If error is returned, assume that Projector is OFF
+                //callback(error);
                 return;
             }
             try {
+                if (debug) {
+                    console.log("-Response\n" + response + "\n-Body\n" + body)
+                }
                 callback(null, JSON.parse(body)["projector"]["feature"]["reply"] === "01")
             } catch (error) {
+                if (debug) {
+                    console.log(error)
+                }
                 callback(error);
             }
         });
     },
 
     setPowerState: function(powerOn, callback) {
-        let command;
+        let uri;
         if (powerOn) {
-            command = "PWR=ON";
+            uri = "http://" + this.irip + "/ir/PROJECTOR_ON";
         } else {
-            command = "PWR=OFF";
+            uri = "http://" + this.ip + command_path + "PWR=OFF";
         }
         if (debug) {
-            console.log(error);
+            console.log("Setting power state for Epson Projector");
         }
         request.get({
-            uri: "http://" + this.ip + command_path + command,
+            uri: uri,
             headers: {
                 "Referer": "http://" + this.ip + "/cgi-bin/webconf",
             },
